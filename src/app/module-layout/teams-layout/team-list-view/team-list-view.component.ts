a7a100e7bdb4312b27d11e5f987ca7e3ef13e532
Alagiri Vimal <avimal@hlth360.net>
2020-11-17 18:49:11 +0530

    Team Updated By Vimal

--- src/app/module-layout/teams-layout/team-list-view/team-list-view.component.ts
+++ src/app/module-layout/teams-layout/team-list-view/team-list-view.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { Subject } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+
+import { Router } from '@angular/router';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { NgiNotificationService } from 'ngi-notification';
+
+import { DOCUMENT } from '@angular/common';
+
+import { CookieService } from 'src/app/core/services/cookie.service';
+
+import { RestApiService } from '../../../shared/rest-api.services';
+
+import { HttpClient } from '@angular/common/http';
+
+//import { TagCrudService } from '../tag-crud.service';
+
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-team-list-view',
@@ -7,9 +29,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamListViewComponent implements OnInit {
 
-  constructor() { }
+  //db: any = firebase.firestore();
+  value: any = [];
+  getAllTeams: any = [];
+  getAllTeamsData: any = [];
 
-  ngOnInit() {
+  teamData:any=[];
+  change:any=[];
+
+  data: any;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
+ 
+  resourceID = this.route.snapshot.paramMap.get('resourceId'); 
+
+  loading = true;
+  displayLoader: any = true;
+
+  uid: any;
+  orgId: any;
+
+
+  constructor(private router: Router, 
+    private route: ActivatedRoute,
+    private notification: NgiNotificationService,
+     @Inject(DOCUMENT) private _document: Document,
+     public cookieService: CookieService, 
+     private restApiService: RestApiService, 
+     private http:HttpClient,
+      private logger: NGXLogger) { }
+
+  ngOnInit() { 
+    this.uid = this.cookieService.getCookie('uid');
+    this.orgId = localStorage.getItem('org_id');
+    this.getTeams();  
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      processing: true
+    };     
   }
+
+  async getTeams(){
+    console.log(this.orgId);
+ 
+      await this.restApiService.lists('teams/'+this.resourceID).subscribe( teamdata =>{
+        this.restApiService.lists('teammembers/'+this.resourceID).subscribe( memberdata =>{
+          this.teamData = {'team': teamdata, 'member': memberdata}
+        });        
+      }, e=>{
+        console.log('----API error for team list----', e)
+      });
+      console.log('---id---', this.resourceID);  
+    
+  }
+ 
+  listTeam(){
+    this.router.navigate(['/teams/list']);
+  }
+
+  addTeam(){
+    this.router.navigate(['/teams/listcreate']);
+  }
+  
+  viewTeam(resourceId: string){
+    this.router.navigate(['/teams/listview/'+resourceId]);
+  }
+  
+  editTeam(resourceId: string){
+    this.router.navigate(['/teams/listedit/'+resourceId]);
+  }
+
+  async deleteTag(resourceId: string, resourceName: string){
+    
+    try {
+      this.notification.isConfirmation('', '', 'Tags Data', ' Are you sure to delete ' + resourceName + ' ?', 'question-circle', 'Yes', 'No', 'custom-ngi-confirmation-wrapper').then(async (dataIndex) => {
+        if (dataIndex[0]) {
+          console.log("yes");
+          //await this.db.collection('Tags').doc(resourceId).delete();
+          this.notification.isNotification(true, "Tags Data", "Tags Data has been deleted successfully.", "check-square");
+          this.refreshPage();
+        } else {
+          console.log("no");
+        }
+      }, (err) => {
+        console.log(err);
+      })
+    } catch (error) {
+      console.log(error);
+      this.notification.isNotification(true, "Tags Data", "Unable to delete.Please try again later.", "times-circle");
+    }
+  }
+ 
+ refreshPage() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(['/teams/list']);
+}
 
 }
--- src/app/module-layout/teams-layout/team-list/team-list.component.ts
+++ src/app/module-layout/teams-layout/team-list/team-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import * as firebase from 'firebase';
+
 import { Subject } from 'rxjs';
 
 import 'rxjs/add/operator/map';
@@ -12,6 +12,14 @@ import { DOCUMENT } from '@angular/common';
 
 import { CookieService } from 'src/app/core/services/cookie.service';
 
+import { RestApiService } from '../../../shared/rest-api.services';
+
+import { HttpClient } from '@angular/common/http';
+
+//import { TagCrudService } from '../tag-crud.service';
+
+import { NGXLogger } from 'ngx-logger';
+
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
@@ -19,26 +27,11 @@ import { CookieService } from 'src/app/core/services/cookie.service';
 })
 export class TeamListComponent implements OnInit {
 
-  db: any = firebase.firestore();
+  //db: any = firebase.firestore();
   value: any = [];
   getAllTeams: any = [];
   getAllTeamsData: any = [];
-  public getAllTagsData:any= [
-    {
-        "tag_id": 1,
-        "col_code_fk": 5,
-        "team_name": "A",
-        "level": 0,
-        "players": "indian",
-        "coaches": "India",
-        "managers": "venkat",
-        "season": "USR",
-        "sport": "USR",
-        "create_date": "2014-02-08 06:13:55",
-        "edit_by": 0,
-        "edit_date": "0000-00-00 00:00:00"
-    }
-]
+  
 
   data: any;
   dtOptions: DataTables.Settings = {};
@@ -51,52 +44,74 @@ export class TeamListComponent implements OnInit {
   orgId: any;
 
 
-  constructor(private router: Router, private notification: NgiNotificationService, @Inject(DOCUMENT) private _document: Document,public cookieService: CookieService) { }
+  constructor(private router: Router, 
+    private notification: NgiNotificationService,
+     @Inject(DOCUMENT) private _document: Document,
+     public cookieService: CookieService, 
+     private restApiService: RestApiService, 
+     private http:HttpClient,
+      private logger: NGXLogger) { }
 
   ngOnInit() { 
-
     this.uid = this.cookieService.getCookie('uid');
     this.orgId = localStorage.getItem('org_id');
-    
+    this.getTeams();  
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10,
       processing: true
-    }; 
-    this.getTeams();  
+    };     
   }
 
   async getTeams(){
     console.log(this.orgId);
+
+
+  await this.restApiService.lists('teams').subscribe( res =>{
+      this.getAllTeamsData = res;
+      console.log("==getAllTeamsData==",this.getAllTeamsData);
+      this.data = res;
+      this.dtTrigger.next();
+      this.loading = false;
+      this.displayLoader = false;  
+    }, e=>{
+      console.log('----API error for team list----', e)
+    })
+
+    /*
+    
     if(this.orgId=='') {
       this.getAllTeams = await this.db.collection('/teams').orderBy('sport_id').get();
     } else {
       this.getAllTeams = await this.db.collection('/teams').where('organization_id', '==', this.orgId).get();
     }
-
     this.getAllTeamsData = await this.getAllTeams.docs.map((doc: any) => doc.data());
+
     this.data = this.getAllTeamsData;
     this.dtTrigger.next();
     this.loading = false;
     this.displayLoader = false; 
  
     console.log(this.data);
+    
+    */
+
 
   }
  
-  listTag(){
+  listTeam(){
     this.router.navigate(['/teams/list']);
   }
 
-  addTag(){
+  addTeam(){
     this.router.navigate(['/teams/listcreate']);
   }
   
-  viewTag(resourceId: string){
+  viewTeam(resourceId: string){
     this.router.navigate(['/teams/listview/'+resourceId]);
   }
   
-  editTag(resourceId: string){
+  editTeam(resourceId: string){
     this.router.navigate(['/teams/listedit/'+resourceId]);
   }
 
@@ -106,7 +121,7 @@ export class TeamListComponent implements OnInit {
       this.notification.isConfirmation('', '', 'Tags Data', ' Are you sure to delete ' + resourceName + ' ?', 'question-circle', 'Yes', 'No', 'custom-ngi-confirmation-wrapper').then(async (dataIndex) => {
         if (dataIndex[0]) {
           console.log("yes");
-          await this.db.collection('Tags').doc(resourceId).delete();
+          //await this.db.collection('Tags').doc(resourceId).delete();
           this.notification.isNotification(true, "Tags Data", "Tags Data has been deleted successfully.", "check-square");
           this.refreshPage();
         } else {
@@ -124,7 +139,7 @@ export class TeamListComponent implements OnInit {
  refreshPage() {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     this.router.onSameUrlNavigation = 'reload';
-    this.router.navigate(['/tags/list']);
+    this.router.navigate(['/teams/list']);
 }
 
 }
